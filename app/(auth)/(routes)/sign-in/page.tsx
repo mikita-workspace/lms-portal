ef0d5fea3ce6ae02393046105589875022d13502
Mikita Kandratsyeu <nickondr.production@gmail.com>
2024-02-02 19:38:02 +0300

    setup of folders

---
+++ app/(auth)/(routes)/sign-in/page.tsx
@@ -0,0 +1,9 @@
+const SignIn = () => {
+  return (
+    <div>
+      <p className="text-3xl font-medium text-sky-500">Unprotected page</p>
+    </div>
+  );
+};
+
+export default SignIn;
---
+++ app/(dashboard)/(routes)/page.tsx
@@ -0,0 +1,9 @@
+const Home = () => {
+  return (
+    <div>
+      <p className="text-3xl font-medium text-sky-500">Protected page</p>
+    </div>
+  );
+};
+
+export default Home;
