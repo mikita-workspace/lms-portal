438c4d5c826ffed7686b0b5e7adde6685a7f7f76
Mikita Kandratsyeu <nickondr.production@gmail.com>
2024-02-02 23:57:19 +0300

    dark mode

--- app/(dashboard)/_components/navbar.tsx
+++ app/(dashboard)/_components/navbar.tsx
@@ -5,7 +5,7 @@ import { MobileSideBar } from './sidebar.mobile';
 
 export const NavBar = () => {
   return (
-    <div className="p-4 border-b h-full flex items-center bg-white shadow-sm">
+    <div className="p-4 border-b h-full flex items-center bg-white dark:bg-slate-800 shadow-sm">
       <MobileSideBar />
       <div className="flex items-center ml-auto">
         <ThemeSwitcher />
