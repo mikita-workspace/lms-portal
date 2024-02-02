7cf7b9e53bb7cc58816ad3c788e178e4aba16c31
Mikita Kandratsyeu <nickondr.production@gmail.com>
2024-02-02 19:23:54 +0300

    setting env

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from '@/components/ui/button';
+
+export default function Home() {
+  return (
+    <div>
+      <p className="text-3xl font-medium text-sky-500">Hello sdfs</p>
+      <Button variant="outline">Click me</Button>
+    </div>
+  );
+}
