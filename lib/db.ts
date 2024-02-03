4e0b1704882e6b9ee2b5e1831a35c1d4855ffe5c
Mikita Kandratsyeu <nickondr.production@gmail.com>
2024-02-03 23:31:39 +0300

    prisma

---
+++ lib/db.ts
@@ -0,0 +1,14 @@
+import { PrismaClient } from '@prisma/client';
+
+// declare global {
+//   // eslint-disable-next-line no-var
+//   var prisma: PrismaClient | undefined;
+// }
+
+// export const db = globalThis.prisma || new PrismaClient();
+
+// if (process.env.NODE_ENV !== 'production') {
+//   globalThis.prisma = db;
+// }
+
+export const db = new PrismaClient();
