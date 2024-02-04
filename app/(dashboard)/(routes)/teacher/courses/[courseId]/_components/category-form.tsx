ded5739dd9b9372690d5bda376bba46277b33ad7
Mikita Kandratsyeu <nickondr.production@gmail.com>
2024-02-04 19:01:44 +0300

    lint fix

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/category-form.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/category-form.tsx
@@ -13,7 +13,6 @@ import * as z from 'zod';
 import { Button } from '@/components/ui/button';
 import { Combobox } from '@/components/ui/combobox';
 import { Form, FormControl, FormField, FormItem, FormMessage } from '@/components/ui/form';
-import { Textarea } from '@/components/ui/textarea';
 import { cn } from '@/lib/utils';
 
 type CategoryFormProps = {
