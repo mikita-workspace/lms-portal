6cf065b5af8a825236e4745941359962cc2b68a4
Mikita Kandratsyeu <nickondr.production@gmail.com>
2024-02-07 12:53:55 +0300

    new folder

--- app/api/courses/[courseId]/attachments/[attachmentId]/route.ts
+++ app/api/courses/[courseId]/attachments/[attachmentId]/route.ts
@@ -2,7 +2,7 @@ import { HttpStatusCode } from 'axios';
 import { NextRequest, NextResponse } from 'next/server';
 
 import { getCurrentUser } from '@/actions/get-current-user';
-import { deleteFiles } from '@/actions/uploadthing-delete-files';
+import { deleteFiles } from '@/actions/uploadthing/delete-files';
 import { db } from '@/lib/db';
 
 export const DELETE = async (
