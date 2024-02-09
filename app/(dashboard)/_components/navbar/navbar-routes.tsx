939da883528a839102baf398d1508e5cd1255060
Mikita Kandratsyeu <nickondr.production@gmail.com>
2024-02-09 09:58:48 +0300

    updated navbaer skeleton

--- app/(dashboard)/_components/navbar/navbar-routes.tsx
+++ app/(dashboard)/_components/navbar/navbar-routes.tsx
@@ -5,12 +5,25 @@ import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 
 import { UserProfileButton } from '@/components/auth/user-profile-button';
-import { Button } from '@/components/ui';
+import { Button, Skeleton } from '@/components/ui';
+import { AuthStatus } from '@/constants/auth';
 import { useCurrentUser } from '@/hooks/use-current-user';
 
 export const NavBarRoutes = () => {
   const pathname = usePathname();
-  const { user } = useCurrentUser();
+  const { user, status } = useCurrentUser();
+
+  if (status === AuthStatus.LOADING) {
+    return (
+      <div className="flex items-center space-x-4 ml-auto">
+        <div className="space-y-2">
+          <Skeleton className="h-3 w-[100px]" />
+          <Skeleton className="h-3 w-[75px]" />
+        </div>
+        <Skeleton className="h-10 w-10 rounded-full" />
+      </div>
+    );
+  }
 
   const isStudentPage = pathname?.includes('/chapter');
   const isTeacherPage = pathname?.startsWith('/teacher');
