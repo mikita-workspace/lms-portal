90b1a90f665124c8214ec1cce45d8971450128ce
Mikita Kandratsyeu <mikita_kandratsyeu1@epam.com>
2024-02-13 17:51:37 +0300

    chore: replaced icon with svg

--- components/loaders/pretty-loader.tsx
+++ components/loaders/pretty-loader.tsx
@@ -6,7 +6,7 @@ const poppins = Poppins({ subsets: ['latin'], weight: ['400', '500'] });
 export const PrettyLoader = () => {
   return (
     <div className="w-full h-full flex items-center justify-center">
-      <Image src="/assets/pretty-loader.gif" alt="Pretty Loader" width={60} height={60} />
+      <Image src="/assets/pretty-loader.svg" alt="Pretty Loader" width={50} height={50} />
       <div className={poppins.className}>
         <p className="font-semibold text-base text-neutral-700 dark:text-neutral-300">Nova LMS</p>
         <p className="text-xs text-muted-foreground">Portal&nbsp;for&nbsp;educational purposes</p>
