6193254ab6d81fe9817339c2b3a3efa83a31e49b
Mikita Kandratsyeu <mikita_kandratsyeu1@epam.com>
2024-02-16 16:28:21 +0300

    chore: added error page

---
+++ app/error.tsx
@@ -0,0 +1,24 @@
+'use client';
+
+import { Button } from '@/components/ui';
+
+type ErrorProps = { error: Error & { digest?: string }; reset: () => void };
+
+const Error = ({ error, reset }: ErrorProps) => {
+  return (
+    <div className="h-full flex gap-y-4 items-center w-full">
+      <div className="flex items-center gap-y-4 flex-col w-full text-muted-foreground">
+        <h1 className="text-xl md:text-3xl font-semibold">500</h1>
+        <div className="flex flex-col items-center gap-1">
+          <p className="text-sm md:text-lg">An unexpected application error has occurred.</p>
+          <p className="text-xs">({error.message})</p>
+        </div>
+        <Button variant="secondary" onClick={() => reset()}>
+          Try again
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+export default Error;
