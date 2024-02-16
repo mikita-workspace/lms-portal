422f4a5e4504d8248affc5905b657d632561e62c
Mikita Kandratsyeu <mikita_kandratsyeu1@epam.com>
2024-02-16 13:43:16 +0300

    chore: added open ai models list from server

--- actions/chat/get-chat-initial.ts
+++ actions/chat/get-chat-initial.ts
@@ -7,8 +7,6 @@ import { openai } from '@/server/openai';
 
 export const getChatInitial = async () => {
   try {
-    const { data: models } = await openai.models.list();
-
     const introMessages = await openai.chat.completions.create({
       messages: [
         {
@@ -27,9 +25,12 @@ export const getChatInitial = async () => {
 
     return {
       introMessages: JSON.parse(introMessages.choices[0].message.content || '[]'),
-      models: models
-        .map((model) => ({ value: model.id, label: model.id }))
-        .filter((model) => model.value.startsWith('gpt')),
+      models: [
+        // { value: 'gpt-4-0125-preview', label: 'GPT-4 Turbo (0125)' },
+        { value: 'gpt-4-1106-preview', label: 'GPT-4 Turbo (1106)' },
+        { value: 'gpt-3.5-turbo-0125', label: 'GPT-3.5 Turbo (0125)' },
+        { value: 'gpt-3.5-turbo-1106', label: 'GPT-3.5 Turbo (1106)' },
+      ],
     };
   } catch (error) {
     console.error('[GET_CHAT_INITIAL_ACTION]', error);
