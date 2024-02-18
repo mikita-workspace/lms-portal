afce2b746f15feea9ca27be3f8acd8f489224344
Mikita Kandratsyeu <nickondr.production@gmail.com>
2024-02-18 17:37:04 +0300

    fixed error page

--- app/error.tsx
+++ app/error.tsx
@@ -9,7 +9,7 @@ const Error = ({ error }: ErrorProps) => {
     <div className="h-full flex gap-y-4 items-center w-full">
       <div className="flex items-center gap-y-4 flex-col w-full text-muted-foreground">
         <h1 className="text-xl md:text-3xl font-semibold">500</h1>
-        <div className="flex flex-col items-center gap-1">
+        <div className="flex flex-col items-center gap-1 px-4 md:max-w-2xl">
           <p className="text-sm md:text-lg">An unexpected application error has occurred.</p>
           {error?.message && <p className="text-xs">({error.message})</p>}
         </div>
