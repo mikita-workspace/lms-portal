49b273c9334cfd629f961ee9599c9cbd528f7707
Mikita Kandratsyeu <nickondr.production@gmail.com>
2024-02-20 13:00:13 +0300

    chore: fixed settings ui

--- components/loaders/billing-skeleton.tsx
+++ components/loaders/billing-skeleton.tsx
@@ -3,7 +3,7 @@
 import { Skeleton } from '../ui/skeleton';
 
 export const BillingSkeleton = () => (
-  <div className="w-full px-6 pt-10 pb-6 flex flex-col mx-auto gap-y-4 md:max-w-2xl">
+  <div className="w-full px-6 pt-10 pb-6 flex flex-col mx-auto gap-y-4">
     <Skeleton className="h-[30px] w-[150px] mb-4" />
     <Skeleton className="h-[40px] mb-4 mt-8" />
     <div className="flex flex-col gap-8">
