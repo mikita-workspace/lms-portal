1ed83c1ed67cc5b30890dc835b01802b20faea7e
Mikita Kandratsyeu <nickondr.production@gmail.com>
2024-02-26 19:20:03 +0300

    tsc fix

--- app/(settings)/(routes)/settings/stripe/page.tsx
+++ app/(settings)/(routes)/settings/stripe/page.tsx
@@ -3,13 +3,13 @@ import { getStripeInfo } from '@/actions/db/get-stripe-info';
 import { StripeBalances } from './_components/stripe-balances';
 
 const StripePage = async () => {
-  const { stripeBalances, stripeTransactions } = await getStripeInfo();
+  const { stripeBalances } = await getStripeInfo();
 
   return (
     <div className="p-6 flex flex-col mb-6">
       <h1 className="text-2xl font-medium">Admin Settings</h1>
       <div className="mt-12">
-        <StripeBalances balances={stripeBalances} transactions={stripeTransactions} />
+        <StripeBalances balances={stripeBalances} />
       </div>
     </div>
   );
