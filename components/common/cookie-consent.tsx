044a5cf45ea4e6b742fda8979a2f4aaa9a58522c
Mikita Kandratsyeu <mikita_kandratsyeu1@epam.com>
2024-02-28 21:27:27 +0300

    chore: added dynamic styles for cookie consent info

--- components/common/cookie-consent.tsx
+++ components/common/cookie-consent.tsx
@@ -5,28 +5,46 @@ import Link from 'next/link';
 import { useEffect, useState } from 'react';
 
 import { COOKIE_POLICY_URL } from '@/constants/common';
+import { cn } from '@/lib/utils';
 
 import { Button } from '../ui';
 
 export const CookieConsent = () => {
-  const [showConsent, setShowConsent] = useState(true);
+  const [shownConsent, setShownConsent] = useState(true);
+  const [isClosing, setIsClosing] = useState(false);
 
   useEffect(() => {
-    setShowConsent(hasCookie('cookie-consent'));
+    const hasShownContent = hasCookie('cookie-consent');
+
+    setShownConsent(hasShownContent);
   }, []);
 
+  useEffect(() => {
+    const body = document.getElementsByTagName('body')[0];
+    body?.setAttribute('style', isClosing || shownConsent ? 'overflow:auto' : 'overflow:hidden');
+  }, [shownConsent, isClosing]);
+
   const handleAcceptCookie = () => {
-    setShowConsent(true);
+    setIsClosing(true);
     setCookie('cookie-consent', 'true', {});
+
+    setTimeout(() => {
+      setShownConsent(true);
+    }, 1000);
   };
 
-  if (showConsent) {
+  if (shownConsent) {
     return null;
   }
 
   return (
-    <div className="fixed inset-0 bg-background/80 backdrop-blur-sm z-50">
-      <div className="fixed bottom-0 left-0 right-0 flex items-center justify-between p-6 bg-background border-t">
+    <div
+      className={cn(
+        'fixed inset-0 bg-background/80 backdrop-blur-sm transition-opacity duration-1000 z-50',
+        isClosing ? 'opacity-0' : 'opacity-100',
+      )}
+    >
+      <div className="fixed bottom-0 left-0 right-0 flex items-center justify-between p-6 bg-background border-t shadow-lg">
         <span className="text-dark text-sm font-medium mr-16">
           We use cookies to enhance the user experience. By using our website, you consent to all
           cookies in accordance with our{' '}
