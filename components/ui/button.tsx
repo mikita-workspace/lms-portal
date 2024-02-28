5ea770195581661b943202e736e115dec060bad4
Mikita Kandratsyeu <mikita_kandratsyeu1@epam.com>
2024-02-28 15:29:54 +0300

    chore: added some style fixes

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -6,7 +6,7 @@ import { BiLoaderAlt } from 'react-icons/bi';
 import { cn } from '@/lib/utils';
 
 const buttonVariants = cva(
-  'inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50',
+  'inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 truncate',
   {
     variants: {
       variant: {
@@ -17,7 +17,7 @@ const buttonVariants = cva(
         outline: 'border border-input bg-background hover:bg-accent hover:text-accent-foreground',
         secondary: 'bg-secondary text-secondary-foreground hover:bg-secondary/80',
         success:
-          'bg-green-700 hover:bg-green-800 dark:bg-green-600 dark:hover:bg-green-700 text-white truncate',
+          'bg-green-700 hover:bg-green-800 dark:bg-green-600 dark:hover:bg-green-700 text-white',
       },
       size: {
         default: 'h-10 px-4 py-2',
