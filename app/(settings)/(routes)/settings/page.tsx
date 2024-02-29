96995df0c5f87010479b54a94d0665c41ffd6cb3
Mikita Kandratsyeu <mikita_kandratsyeu1@epam.com>
2024-02-29 15:03:02 +0300

    chore: changed max-w for general settings page

--- app/(settings)/(routes)/settings/page.tsx
+++ app/(settings)/(routes)/settings/page.tsx
@@ -9,7 +9,7 @@ const SettingsPage = async () => {
   const userInfo = await db.user.findUnique({ where: { id: user?.userId } });
 
   return (
-    <div className="p-6 flex flex-col mb-6 md:max-w-2xl">
+    <div className="p-6 flex flex-col mb-6 md:max-w-[868px]">
       <h1 className="text-2xl font-medium">General Settings</h1>
       <div className="mt-12">
         <GeneralSettingsForm initialData={userInfo!} />
