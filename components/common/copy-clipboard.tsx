3ab38fcca2eb6b38ee444927824222ebd9e1f15b
Mikita Kandratsyeu <nickondr.production@gmail.com>
2024-03-07 19:59:23 +0300

    updated copy button

---
+++ components/common/copy-clipboard.tsx
@@ -0,0 +1,29 @@
+'use client';
+
+import { Copy } from 'lucide-react';
+import { useState } from 'react';
+
+import { cn } from '@/lib/utils';
+
+type CopyClipboardProps = {
+  textToCopy: string;
+};
+
+export const CopyClipboard = ({ textToCopy }: CopyClipboardProps) => {
+  const [copied, setCopied] = useState(false);
+
+  const handleCopyToClipboard = () => {
+    setCopied(true);
+    navigator.clipboard.writeText(textToCopy);
+
+    setTimeout(() => {
+      setCopied(false);
+    }, 1000);
+  };
+
+  return (
+    <button onClick={handleCopyToClipboard}>
+      <Copy className={cn('h-4 w-4', copied ? 'animate-spin-once' : '')} />
+    </button>
+  );
+};
