f9dcf0dde91b771f18db08e5f30e51eb627b2695
Mikita Kandratsyeu <nickondr.production@gmail.com>
2024-03-08 13:14:18 +0300

    chore: improved notifications experience

--- components/notifications/notification-cards.tsx
+++ components/notifications/notification-cards.tsx
@@ -17,21 +17,22 @@ type NotificationCardsProps = {
   notifications: Omit<Notification, 'userId'>[];
 };
 
-export const NotificationCards = ({
-  isFetching = false,
-  notifications,
-}: NotificationCardsProps) => {
-  const { user } = useCurrentUser();
+type NotificationCardProps = Omit<NotificationCardsProps, 'notifications'> & {
+  notification: Omit<Notification, 'userId'>;
+  userId?: string;
+};
+
+const NotificationCard = ({ isFetching = false, notification, userId }: NotificationCardProps) => {
   const router = useRouter();
   const [pending, startTransition] = useTransition();
 
-  const [notificationId, setNotificationId] = useState<string | null>(null);
+  const [isLoading, setIsLoading] = useState(false);
 
   const handleMarkAsRead = async (id: string) => {
     try {
-      setNotificationId(id);
+      setIsLoading(true);
 
-      await fetcher.patch(`/api/users/${user?.userId}/notifications`, {
+      await fetcher.patch(`/api/users/${userId}/notifications`, {
         body: {
           id,
           isRead: true,
@@ -42,43 +43,56 @@ export const NotificationCards = ({
     } catch (error) {
       toast.error('Something went wrong!');
     } finally {
-      setNotificationId(null);
+      setIsLoading(false);
     }
   };
 
   return (
-    <>
-      {notifications.map((un) => (
-        <div
-          key={un.id}
-          className={cn(
-            'border rounded-sm p-3 mb-2 flex flex-col',
-            un.isRead ? 'text-muted-foreground' : '',
-          )}
-        >
-          <p className="text-sm font-medium">{un.title}</p>
-          <p className="text-xs">{un.body}</p>
-          <div className="text-xs text-muted-foreground mt-2 flex justify-between items-center">
-            <p>{formatDistanceToNow(un.createdAt, { addSuffix: true })}</p>
-            {un.isRead && <CheckCheck className="h-4 w-4" />}
-            {!un.isRead && (
-              <>
-                {(notificationId === un.id || pending) && (
-                  <BiLoaderAlt className="h-4 w-4 animate-spin" />
-                )}
-                {notificationId !== un.id && !pending && (
-                  <button
-                    className="hover:underline"
-                    onClick={() => handleMarkAsRead(un.id)}
-                    disabled={Boolean(notificationId) || isFetching}
-                  >
-                    Mark as read
-                  </button>
-                )}
-              </>
+    <div
+      className={cn(
+        'border rounded-sm p-3 mb-2 flex flex-col',
+        notification.isRead ? 'text-muted-foreground' : '',
+      )}
+    >
+      <p className="text-sm font-medium">{notification.title}</p>
+      <p className="text-xs">{notification.body}</p>
+      <div className="text-xs text-muted-foreground mt-2 flex justify-between items-center">
+        <p>{formatDistanceToNow(notification.createdAt, { addSuffix: true })}</p>
+        {notification.isRead && <CheckCheck className="h-4 w-4" />}
+        {!notification.isRead && (
+          <>
+            {(isLoading || pending) && <BiLoaderAlt className="h-4 w-4 animate-spin" />}
+            {!isLoading && !pending && (
+              <button
+                className="hover:underline"
+                onClick={() => handleMarkAsRead(notification.id)}
+                disabled={isLoading || isFetching}
+              >
+                Mark as read
+              </button>
             )}
-          </div>
-        </div>
+          </>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export const NotificationCards = ({
+  isFetching = false,
+  notifications,
+}: NotificationCardsProps) => {
+  const { user } = useCurrentUser();
+
+  return (
+    <>
+      {notifications.map((notification) => (
+        <NotificationCard
+          key={notification.id}
+          isFetching={isFetching}
+          notification={notification}
+          userId={user?.userId}
+        />
       ))}
     </>
   );
