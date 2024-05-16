9eb6b219e5a18c625bc48e3865c31ff059419dce
Mikita Kandratsyeu <nickondr.production@gmail.com>
2024-05-16 23:43:52 +0300

    chore: hardcoded captcha code

--- components/common/captcha.tsx
+++ components/common/captcha.tsx
@@ -1,23 +1,16 @@
 'use client';
 
-// import { SmartCaptcha } from '@yandex/smart-captcha';
+import { SmartCaptcha } from '@yandex/smart-captcha';
 
 type CaptchaProps = { callback: (token: string | null) => void };
 
 export const Captcha = ({ callback }: CaptchaProps) => {
-  callback('token');
-  console.log({
-    NEXT_PUBLIC_PUSHER_KEY: process.env.NEXT_PUBLIC_PUSHER_KEY,
-    NEXT_PUBLIC_YCAPTCHA: process.env.NEXT_PUBLIC_YCAPTCHA,
-  });
-  console.log(process.env);
   return (
-    <div>Test</div>
-    // <SmartCaptcha
-    //   language="en"
-    //   onSuccess={callback}
-    //   sitekey={process.env.NEXT_PUBLIC_YCAPTCHA as string}
-    //   webview
-    // />
+    <SmartCaptcha
+      language="en"
+      onSuccess={callback}
+      sitekey="ysc1_Mff0gcku68UrZdM7anNxqg2cTSM1HZqKKXLe8ibQd0d9bec1"
+      webview
+    />
   );
 };
