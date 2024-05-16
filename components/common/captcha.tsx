3c57cf4260d5b2668dd6faa9f36ecf11b69bc2cf
Mikita Kandratsyeu <nickondr.production@gmail.com>
2024-05-16 23:34:27 +0300

    chore: updated public var

--- components/common/captcha.tsx
+++ components/common/captcha.tsx
@@ -7,14 +7,14 @@ type CaptchaProps = { callback: (token: string | null) => void };
 export const Captcha = ({ callback }: CaptchaProps) => {
   console.log({
     NEXT_PUBLIC_PUSHER_KEY: process.env.NEXT_PUBLIC_PUSHER_KEY,
-    NEXT_PUBLIC_YCAPTCHA_SITEKEY: process.env.NEXT_PUBLIC_CAPTCHA_KEY,
+    NEXT_PUBLIC_YCAPTCHA: process.env.NEXT_PUBLIC_YCAPTCHA,
   });
   console.log(process.env);
   return (
     <SmartCaptcha
       language="en"
       onSuccess={callback}
-      sitekey={process.env.NEXT_PUBLIC_YCAPTCHA_SITEKEY as string}
+      sitekey={process.env.NEXT_PUBLIC_YCAPTCHA as string}
       webview
     />
   );
