e373afacfbac9ee8cb65c2300ca79d8c09ce832a
Mikita Kandratsyeu <nickondr.production@gmail.com>
2024-05-16 22:51:54 +0300

    chore: added yd captcha for deleting account

---
+++ components/common/captcha.tsx
@@ -0,0 +1,16 @@
+'use client';
+
+import { SmartCaptcha } from '@yandex/smart-captcha';
+
+type CaptchaProps = { callback: (token: string | null) => void };
+
+export const Captcha = ({ callback }: CaptchaProps) => {
+  return (
+    <SmartCaptcha
+      language="en"
+      onSuccess={callback}
+      sitekey={process.env.NEXT_PUBLIC_YCAPTCHA_SITEKEY as string}
+      webview
+    />
+  );
+};
