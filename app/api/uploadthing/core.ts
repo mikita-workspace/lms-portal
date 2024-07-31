dacc9b4c8aa367da53b413b89f15c71591cc8b18
Mikita Kandratsyeu <mikita_kandratsyeu1@epam.com>
2024-07-31 11:19:32 +0300

    chore: updated deps

--- app/api/uploadthing/core.ts
+++ app/api/uploadthing/core.ts
@@ -17,7 +17,7 @@ const handleAuth = async () => {
 };
 
 export const ourFileRouter = {
-  courseImage: f({ image: { maxFileSize: '4MB', maxFileCount: 1 } })
+  courseImage: f({ image: { maxFileSize: '2MB', maxFileCount: 1 } })
     .middleware(() => handleAuth())
     .onUploadComplete(() => {}),
   courseAttachments: f({
