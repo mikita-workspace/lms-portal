7c033c7d36a48782dc8b8d49788d1a08d44753ec
Mikita Kandratsyeu <nickondr.production@gmail.com>
2024-08-15 11:14:26 +0300

    code improvements

--- app/(dashboard)/(routes)/settings/billing/page.tsx
+++ app/(dashboard)/(routes)/settings/billing/page.tsx
@@ -1,5 +1,6 @@
 import { getCurrentUser } from '@/actions/auth/get-current-user';
 import { getUserBilling } from '@/actions/stripe/get-user-billing';
+// import { getUserSubscription } from '@/actions/stripe/get-user-subscription';
 import { DataTable } from '@/components/data-table/data-table';
 
 import { columns } from './_components/data-table/columns';
@@ -7,10 +8,11 @@ import { columns } from './_components/data-table/columns';
 const BillingPage = async () => {
   const user = await getCurrentUser();
   const userBilling = await getUserBilling(user?.userId);
+  // const userSubscription = await getUserSubscription(user?.userId, true);
 
   return (
     <div className="p-6 flex flex-col">
-      <h1 className="text-2xl font-medium">Billing History</h1>
+      <h1 className="text-2xl font-medium">Billing & Subscription</h1>
       <div className="mt-12">
         <DataTable
           columns={columns}
---
+++ lib/auth/callbacks.ts
@@ -0,0 +1,81 @@
+import { cookies } from 'next/headers';
+import { NextAuthOptions } from 'next-auth';
+
+import { getUpdatedUser } from '@/actions/auth/get-updated-user';
+import { loginUser } from '@/actions/auth/login-user';
+import { Provider, UserRole } from '@/constants/auth';
+import { OTP_SECRET_SECURE } from '@/constants/otp';
+
+import { isString } from '../guard';
+import { encrypt } from '../utils';
+
+export const callbacks: NextAuthOptions['callbacks'] = {
+  async signIn({ user, account }) {
+    const email = user?.email ?? account?.email;
+    const hasOtpSecret = cookies().has(OTP_SECRET_SECURE);
+
+    if (Object.values(Provider).includes(account?.provider as Provider) && isString(email)) {
+      const dbUser = await loginUser(email, user.name, user.image);
+
+      if (!hasOtpSecret && dbUser.otpSecret) {
+        return `/otp-verification?code=${encodeURIComponent(encrypt({ secret: dbUser.otpSecret, userId: dbUser.id, provider: account?.provider }, process.env.OTP_SECRET as string))}`;
+      }
+
+      user.email = email;
+      user.hasSubscription = dbUser.hasSubscription;
+      user.id = dbUser.id;
+      user.image = dbUser.image;
+      user.isPublic = Boolean(dbUser.isPublic);
+      user.name = dbUser.name;
+      user.role = dbUser.role;
+
+      return true;
+    }
+
+    return false;
+  },
+  async jwt({ token, user, trigger, session }) {
+    if (user) {
+      token.email = user.email;
+      token.hasSubscription = user.hasSubscription;
+      token.isPublic = user.isPublic;
+      token.role = user.role;
+    }
+
+    if (trigger === 'update') {
+      if (session?.name) {
+        token.name = session.name;
+      }
+
+      if (session?.pictureUrl) {
+        token.picture = session.pictureUrl;
+      }
+    }
+
+    return token;
+  },
+  async session({ session, token }) {
+    if (session.user) {
+      if (token.sub) {
+        session.user.email = token.email;
+        session.user.hasSubscription = Boolean(token.subscription);
+        session.user.isPublic = Boolean(token.isPublic);
+        session.user.userId = token.sub;
+      }
+
+      if (token.role) {
+        session.user.role = (token.role as string) || UserRole.STUDENT;
+      }
+
+      const userId = session?.user?.userId;
+
+      const updatedToken = await getUpdatedUser(userId);
+
+      if (updatedToken?.role && updatedToken.role !== session?.user?.role) {
+        session.user.role = updatedToken.role;
+      }
+    }
+
+    return session;
+  },
+};
