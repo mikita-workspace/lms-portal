bf349b5d8515f4eded1e8f3d1b66323dd15e79d6
Mikita Kandratsyeu <nickondr.production@gmail.com>
2024-10-12 13:34:08 +0300

    chore: added caching for chat initial messages

--- actions/chat/get-chat-initial.ts
+++ actions/chat/get-chat-initial.ts
@@ -3,27 +3,37 @@
 import { getLocale } from 'next-intl/server';
 import { ChatCompletionUserMessageParam } from 'openai/resources/index.mjs';
 
+import { ONE_DAY_SEC } from '@/constants/common';
 import { ChatCompletionRole, DEFAULT_MODEL } from '@/constants/open-ai';
+import { fetchCachedData } from '@/lib/cache';
 import { openai } from '@/server/openai';
 
 export const getChatInitial = async () => {
   const locale = await getLocale();
 
   try {
-    const introMessages = await openai.chat.completions.create({
-      messages: [
-        {
-          role: 'system',
-          content: 'You are a machine that only returns array format.',
-        },
-        {
-          content: `Generate 4 questions ranging from 120 to 150 characters long for an intelligent chat in ${locale} language. Write the result to an array.`,
-          role: ChatCompletionRole.USER as unknown as ChatCompletionUserMessageParam['role'],
-        },
-      ],
-      model: DEFAULT_MODEL,
-      temperature: 0.8,
-    });
+    const introMessages = await fetchCachedData(
+      `chat-initial-[${locale}]`,
+      async () => {
+        const response = await openai.chat.completions.create({
+          messages: [
+            {
+              role: 'system',
+              content: 'You are a machine that only returns array format.',
+            },
+            {
+              content: `Generate 4 questions ranging from 120 to 150 characters long for an intelligent chat in ${locale} language. Write the result to an array.`,
+              role: ChatCompletionRole.USER as unknown as ChatCompletionUserMessageParam['role'],
+            },
+          ],
+          model: DEFAULT_MODEL,
+          temperature: 0.8,
+        });
+
+        return response;
+      },
+      ONE_DAY_SEC,
+    );
 
     return {
       introMessages: JSON.parse(introMessages.choices[0].message.content || '[]'),
