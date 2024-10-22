85c282d70846d8013907095a986c8ed21fcbc544
Mikita Kandratsyeu <nickondr.production@gmail.com>
2024-10-22 23:14:47 +0300

    bug: updated path for legal docs

--- lib/legals.ts
+++ lib/legals.ts
@@ -3,7 +3,7 @@ import { readFileSync } from 'node:fs';
 import path from 'path';
 
 export const getLegalsDocument = (filename: string) => {
-  const filePath = path.join('legals/', `${filename}.md`);
+  const filePath = path.join('docs', `${filename}.md`);
   const fileContent = readFileSync(filePath, 'utf8');
 
   return fileContent;
