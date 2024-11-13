d14ebe837073fcd6cc0e50281f633b94998559ce
Mikita Kandratsyeu <nickondr.production@gmail.com>
2024-11-13 17:37:33 +0300

    code refactor

--- app/(chat)/(routes)/chat/[[...slug]]/_components/chat-sidebar/chat-sidebar-items.tsx
+++ app/(chat)/(routes)/chat/[[...slug]]/_components/chat-sidebar/chat-sidebar-items.tsx
@@ -1,25 +1,39 @@
 'use client';
 
-import { GlobeLock } from 'lucide-react';
+import { DragDropContext, Draggable, Droppable, DropResult } from '@hello-pangea/dnd';
+import { EllipsisVertical, GlobeLock, GripVertical, Pencil, Trash2 } from 'lucide-react';
+import { useRouter } from 'next/navigation';
+import { useTranslations } from 'next-intl';
 import { SyntheticEvent, useEffect, useState } from 'react';
 
 import { Conversation } from '@/actions/chat/get-chat-conversations';
-import { Input } from '@/components/ui';
+import {
+  Button,
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from '@/components/ui';
+import { useToast } from '@/components/ui/use-toast';
 import { useChatStore } from '@/hooks/use-chat-store';
 import { getChatMessages } from '@/lib/chat';
+import { fetcher } from '@/lib/fetcher';
 import { cn } from '@/lib/utils';
-
-import { ConversationActions } from './conversation-actions';
 
 type ChatSideBarItemsProps = {
   conversations: Conversation[];
 };
 
-//FIXME: Add Edit and remove functionality
 export const ChatSideBarItems = ({ conversations }: ChatSideBarItemsProps) => {
+  const { toast } = useToast();
+  const t = useTranslations('chat.conversation');
+
+  const router = useRouter();
+
   const { conversationId, setConversationId, chatMessages, setChatMessages } = useChatStore();
 
   const [editTitleId, setEditTitleId] = useState('');
+  const [isFetching, setIsFetching] = useState(false);
 
   useEffect(() => {
     if (Object.keys(chatMessages).length !== conversations.length) {
@@ -39,35 +53,99 @@ export const ChatSideBarItems = ({ conversations }: ChatSideBarItemsProps) => {
     }
   };
 
-  return conversations.map(({ id, title }) => {
-    const isActive = conversationId === id;
+  const handleRemoveConversation = async (id: string) => {
+    setIsFetching(true);
 
-    return (
-      <div
-        key={id}
-        className={cn(
-          'flex justify-between items-center transition-all duration-300 hover:bg-muted border-b last:border-none pr-2 hover:cursor-pointer',
-          isActive && 'text-primary bg-muted',
-        )}
-      >
-        <button
-          className="flex flex-1 justify-between items-center gap-x-2 text-muted-foreground text-sm font-[500] pl-4"
-          type="button"
-          onClick={(event) => handleOnClick(event, id)}
-        >
-          <div className="flex items-center gap-x-2 py-5">
-            <GlobeLock
-              className={cn('text-muted-foreground', isActive && 'text-primary animate-spin-once')}
-              size={22}
-            />
-            <p className="text-left line-clamp-2">
-              {editTitleId === id && <Input value={title} />}
-              {editTitleId !== id && title}
-            </p>
+    try {
+      await fetcher.delete(`/api/chat/conversation/${id}`);
+      router.refresh();
+    } catch (error) {
+      toast({ isError: true });
+    } finally {
+      setIsFetching(false);
+    }
+  };
+
+  const handleDragEnd = (result: DropResult) => {
+    if (!result.destination) {
+      return;
+    }
+  };
+
+  return (
+    <DragDropContext onDragEnd={handleDragEnd}>
+      <Droppable droppableId="chapters">
+        {(provided) => (
+          <div {...provided.droppableProps} ref={provided.innerRef}>
+            {conversations.map((conversation, index) => {
+              const isActive = conversationId === conversation.id;
+
+              return (
+                <Draggable key={conversation.id} draggableId={conversation.id} index={index}>
+                  {(innerProvided) => (
+                    <div
+                      {...innerProvided.draggableProps}
+                      className={cn(
+                        'flex justify-between items-center transition-all duration-300 hover:bg-muted pr-2 hover:cursor-pointer text-sm text-muted-foreground font-[500] px-3 py-6',
+                        isActive && 'text-primary bg-muted',
+                      )}
+                      ref={innerProvided.innerRef}
+                      onClick={(event) => handleOnClick(event, conversation.id)}
+                    >
+                      <div
+                        {...innerProvided.dragHandleProps}
+                        className="flex justify-between items-center gap-x-2 pr-2"
+                      >
+                        <GripVertical
+                          className={cn(
+                            'text-muted-foreground h-5 w-5',
+                            isActive && 'text-primary animate-spin-once',
+                          )}
+                        />
+                      </div>
+                      <div className="flex items-center gap-x-2">
+                        <GlobeLock className="w-4 h-4" />
+                        <span> {conversation.title}</span>
+                      </div>
+                      <div className="ml-auto flex items-center gap-x-2">
+                        {isActive && (
+                          <DropdownMenu>
+                            <DropdownMenuTrigger asChild>
+                              <Button
+                                className="h-4 w-4 p-0 outline-none"
+                                variant="ghost"
+                                disabled={isFetching}
+                              >
+                                <EllipsisVertical className="w-4 h-4 cursor-pointer hover:opacity-75 transition duration-300" />
+                                <span className="sr-only">Open menu</span>
+                              </Button>
+                            </DropdownMenuTrigger>
+                            <DropdownMenuContent align="end">
+                              <DropdownMenuItem className="hover:cursor-pointer">
+                                <Pencil className="h-4 w-4  mr-2" />
+                                {t('edit')}
+                              </DropdownMenuItem>
+                              <DropdownMenuItem
+                                className="hover:cursor-pointer text-red-500"
+                                onClick={() => handleRemoveConversation(conversation.id)}
+                                disabled={isFetching}
+                              >
+                                <Trash2 className="h-4 w-4 mr-2" />
+                                {t('remove')}
+                              </DropdownMenuItem>
+                            </DropdownMenuContent>
+                          </DropdownMenu>
+                        )}
+                      </div>
+                    </div>
+                  )}
+                </Draggable>
+              );
+            })}
+            {provided.placeholder}
           </div>
-        </button>
-        {conversationId && <ConversationActions id={id} setEditTitleId={setEditTitleId} />}
-      </div>
-    );
-  });
+        )}
+      </Droppable>
+    </DragDropContext>
+  );
 };
