ba27e847a184c4d1b6c8b0cdd0e95784e450476b
Mikita Kandratsyeu <nickondr.production@gmail.com>
2024-11-16 17:43:50 +0300

    refactor

--- components/modals/chat-conversation-modal.tsx
+++ components/modals/chat-conversation-modal.tsx
@@ -4,7 +4,7 @@ import { zodResolver } from '@hookform/resolvers/zod';
 import { Link, RefreshCcw } from 'lucide-react';
 import { useRouter } from 'next/navigation';
 import { useTranslations } from 'next-intl';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import { useForm } from 'react-hook-form';
 import { z } from 'zod';
 
