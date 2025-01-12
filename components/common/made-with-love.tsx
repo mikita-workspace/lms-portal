08f2ec31cd4ff0dc9eccf11c123afc3a03b0f42f
Mikita Kandratsyeu <nickondr.production@gmail.com>
2025-01-12 16:13:49 +0300

    chore: added debug info

--- components/common/made-with-love.tsx
+++ components/common/made-with-love.tsx
@@ -4,13 +4,18 @@ import { FaHeart } from 'react-icons/fa';
 
 import { cn } from '@/lib/utils';
 
+import packageJson from '../../package.json';
+
 type MadeWithLoveProps = {
   className?: string;
 };
 
 export const MadeWithLove = ({ className }: MadeWithLoveProps) => {
   return (
-    <div className={cn(className, 'flex items-center')}>
+    <div
+      className={cn(className, 'flex items-center')}
+      title={`${packageJson?.name}::${packageJson?.version}`}
+    >
       <span>Made&nbsp;with&nbsp;</span>
       <FaHeart className="text-red-400 h-3 w-3" />
       <span>&nbsp;in&nbsp;Belarus</span>
