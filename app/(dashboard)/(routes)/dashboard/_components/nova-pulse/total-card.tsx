2d68314abde88b12dfbc2b5597611d1b6f5aa897
Mikita Kandratsyeu <nickondr.production@gmail.com>
2025-07-08 21:33:04 +0300

    fix tsc

--- app/(dashboard)/(routes)/dashboard/_components/nova-pulse/total-card.tsx
+++ app/(dashboard)/(routes)/dashboard/_components/nova-pulse/total-card.tsx
@@ -2,20 +2,18 @@
 
 import CountUp from 'react-countup';
 
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui';
+import { Card, CardContent, CardDescription, CardTitle } from '@/components/ui';
 import { DEFAULT_CURRENCY, DEFAULT_LOCALE } from '@/constants/locale';
-import { formatPrice, getConvertedPrice, getCurrencySymbol } from '@/lib/format';
+import { getConvertedPrice, getCurrencySymbol } from '@/lib/format';
 
 type TotalCardProps = {
   type: 'money' | 'time';
 };
 
 export const TotalCard = ({ type }: TotalCardProps) => {
-  const defaultLocale = { locale: DEFAULT_LOCALE, currency: DEFAULT_CURRENCY };
-
   return (
     <Card className="shadow-none h-full p-6">
-      <CardTitle className="mb-2">Total time spent</CardTitle>
+      <CardTitle className="mb-2">{type}</CardTitle>
       <CardDescription className="text-xs mb-4">Your exercise minutes</CardDescription>
       <CardContent className="m-0 p-0">
         <CountUp
