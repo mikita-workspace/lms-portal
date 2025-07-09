03727e91b8856ec0f4130d5b11b2b36a5c0bb4b1
Mikita Kandratsyeu <nickondr.production@gmail.com>
2025-07-09 17:31:21 +0300

    duration feat

--- lib/date.ts
+++ lib/date.ts
@@ -1,4 +1,4 @@
-import { addSeconds } from 'date-fns';
+import { addSeconds, formatDuration, intervalToDuration } from 'date-fns';
 
 export const roundDate = (date: Date) => {
   date.setHours(0);
@@ -6,4 +6,12 @@ export const roundDate = (date: Date) => {
   date.setSeconds(0);
 
   return addSeconds(date, date.getTimezoneOffset() * -1 * 60);
+};
+
+export const formatTimeInSeconds = (seconds: number): string => {
+  const duration = intervalToDuration({ start: 0, end: seconds * 1000 });
+
+  const formattedDuration = formatDuration(duration);
+
+  return formattedDuration ?? '0 minutes';
 };
