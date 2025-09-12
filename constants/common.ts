8f3428e655bfd4ac994711ce67b5f2d647aac803
Mikita Kandratsyeu <nickondr.production@gmail.com>
2025-09-12 10:55:57 +0300

    email for login

--- constants/common.ts
+++ constants/common.ts
@@ -7,6 +7,7 @@ export const ONE_YEAR_SEC = 60 * 60 * 24 * 365;
 export const TEN_MINUTE_SEC = 60 * 10;
 
 export const ONE_DAY_MS = 24 * 60 * 60 * 1000;
+export const ONE_MIN_MS = 60 * 1000;
 
 export const TIMESTAMP_TEMPLATE = 'HH:mm, dd MMM yyyy';
 export const TIMESTAMP_SUBSCRIPTION_TEMPLATE = 'dd MMM yyyy';
