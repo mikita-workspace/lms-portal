601b0cf4a026af1dd497127bfc21d6a579edb67a
Mikita Kandratsyeu <nickondr.production@gmail.com>
2025-09-23 23:05:16 +0300

    chore: updated transaction logic

--- app/api/webhook/stripe/route.ts
+++ app/api/webhook/stripe/route.ts
@@ -1,11 +1,10 @@
-import { format, fromUnixTime } from 'date-fns';
+import { fromUnixTime } from 'date-fns';
 import { StatusCodes } from 'http-status-codes';
 import { headers } from 'next/headers';
 import { NextRequest, NextResponse } from 'next/server';
 import Stripe from 'stripe';
 
 import { sentEmailByTemplate } from '@/actions/mailer/sent-email-by-template';
-import { TIMESTAMP_EMAIL_TEMPLATE } from '@/constants/common';
 import { EMAIL_COURSE_PURCHASE_SUBJECT } from '@/constants/email-subject';
 import { DEFAULT_LANGUAGE } from '@/constants/locale';
 import { removeValueFromMemoryCache } from '@/lib/cache';
@@ -126,7 +125,7 @@ export const POST = async (req: NextRequest) => {
                 {
                   content: Buffer.from(pdfBuffer),
                   contentType: 'application/pdf',
-                  filename: `${session?.metadata?.email}_${format(new Date(), TIMESTAMP_EMAIL_TEMPLATE)}_invoice.pdf`,
+                  filename: `${invoiceId}_invoice.pdf`,
                 },
               ]
             : [],
